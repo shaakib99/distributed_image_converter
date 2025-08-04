e5fb3deb83883d6e1894307c675b7b0cbd88b0ab
Wahid Shakib <wsakib87@gmail.com>
2025-08-04 15:21:35 +0600

    added mysql service

---
+++ database_service/mysql_service/service.py
@@ -0,0 +1,52 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from pydantic import BaseModel
+
+class MySQLService:
+    def __init__(self, database_url: str):
+        self.engine = create_engine(database_url)
+        self.Session = sessionmaker(bind=self.engine)
+
+    async def create_one(self, schema: DeclarativeBase, data: BaseModel) -> DeclarativeBase:
+        async with self.Session() as session:
+            instance = schema(**data.dict())
+            session.add(instance)
+            await session.commit()
+            return instance
+    
+    async def get_one(self, schema: DeclarativeBase, id: str) -> DeclarativeBase:
+        async with self.Session() as session:
+            instance = await session.get(schema, id)
+            return instance
+    
+    async def get_all(self, schema: DeclarativeBase, query: BaseModel) -> list[DeclarativeBase]:
+        async with self.Session() as session:
+            instances = await session.query(schema).filter_by(**query.dict()).all()
+            return instances
+
+    async def update_one(self, schema: DeclarativeBase, id: str, data: BaseModel) -> DeclarativeBase:
+        async with self.Session() as session:
+            instance = await self.get_one(schema, id)
+            for key, value in data.dict().items():
+                setattr(instance, key, value)
+            await session.commit()
+            return instance
+
+    async def delete_one(self, schema: DeclarativeBase, id: str) -> None:
+        async with self.Session() as session:
+            instance = await self.get_one(schema, id)
+            await session.delete(instance)
+            await session.commit()
+    
+    async def create_metadata(self, schema: DeclarativeBase):
+        async with self.Session() as session:
+            schema.metadata.create_all(self.engine)
+
+class MySQLSingleton:
+    _instance = None
+
+    @classmethod
+    def get_instance(cls, database_url: str):
+        if cls._instance is None:
+            cls._instance = MySQLService(database_url)
+        return cls._instance
