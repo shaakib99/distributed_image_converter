e37cb96be97fe42f4f7b6e29612bb9e450e27592
Wahid Shakib <wsakib87@gmail.com>
2025-08-19 12:35:50 +0600

    added rabbitmq service

---
+++ file_processing_service/rabbitmq_service/service.py
@@ -0,0 +1,45 @@
+
+from aio_pika import connect_robust, Message
+
+
+class RabbitMQService:
+    def __init__(self) -> None:
+        self._connection = None
+        self._channel = None
+
+    async def connect(self, uri: str) -> None:
+        self._connection = await connect_robust(uri)
+        self._channel = await self._connection.channel()
+
+    async def publish(self, target_exchange: str, routing_key: str, body: bytes) -> None:
+        if not self._channel:
+            raise RuntimeError("Publisher is not connected")
+        exchange = await self._channel.get_exchange(target_exchange)
+        await exchange.publish(Message(body), routing_key=routing_key)
+
+    async def declare_queue(self, queue_name: str) -> None:
+        if not self._channel:
+            raise RuntimeError("Publisher is not connected")
+        await self._channel.declare_queue(queue_name)
+    
+    async def declare_exchange(self, exchange_name: str, exchange_type: str) -> None:
+        if not self._channel:
+            raise RuntimeError("Publisher is not connected")
+        await self._channel.declare_exchange(exchange_name, exchange_type)
+
+    async def bind_exchange_queue(self, exchange_name: str, queue_name: str) -> None:
+        if not self._channel:
+            raise RuntimeError("Publisher is not connected")
+        exchange = await self._channel.get_exchange(exchange_name)
+        if exchange is None:
+            raise ValueError(f"Exchange '{exchange_name}' not found")
+        queue = await self._channel.get_queue(queue_name)
+        if queue is None:
+            raise ValueError(f"Queue '{queue_name}' not found")
+        await queue.bind(exchange)
+
+    async def close(self) -> None:
+        if self._channel:
+            await self._channel.close()
+        if self._connection:
+            await self._connection.close()
