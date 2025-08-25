a2f087e4917bb859eb6bf0bb4eaa51c65dc890ab
Wahid Shakib <wsakib87@gmail.com>
2025-08-25 14:57:12 +0600

    workers and rabbitmq services are completed

--- file_processing_service/rabbitmq_service/service.py
+++ file_processing_service/rabbitmq_service/service.py
@@ -1,14 +1,16 @@
 
 from aio_pika import connect_robust, Message
+import os
 
 
 class RabbitMQService:
     def __init__(self) -> None:
         self._connection = None
         self._channel = None
+        self.uri = os.getenv("RABBITMQ_URI", "amqp://guest:guest@localhost/")
 
-    async def connect(self, uri: str) -> None:
-        self._connection = await connect_robust(uri)
+    async def connect(self) -> None:
+        self._connection = await connect_robust(self.uri)
         self._channel = await self._connection.channel()
 
     async def publish(self, target_exchange: str, routing_key: str, body: bytes) -> None:
@@ -16,6 +18,12 @@ class RabbitMQService:
             raise RuntimeError("Publisher is not connected")
         exchange = await self._channel.get_exchange(target_exchange)
         await exchange.publish(Message(body), routing_key=routing_key)
+
+    async def consume(self, queue_name: str, auto_ack=True, on_message = lambda x: x) -> None:
+        if not self._channel:
+            raise RuntimeError("Consumer is not connected")
+        queue = await self._channel.get_queue(queue_name)
+        await queue.consume(callback=on_message, no_ack=auto_ack)
 
     async def declare_queue(self, queue_name: str) -> None:
         if not self._channel:
@@ -43,3 +51,10 @@ class RabbitMQService:
             await self._channel.close()
         if self._connection:
             await self._connection.close()
+
+class RabbitMQServiceSingleton:
+    _instance = None
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = RabbitMQService()
+        return cls._instance
